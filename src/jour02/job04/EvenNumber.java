5b3fda2b8164e54ec91e1b12a2d7ed31664b4f53
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-05-13 19:02:23 +0200

    [ADD] runtrack job04-day02

---
+++ src/jour02/job04/EvenNumber.java
@@ -0,0 +1,11 @@
+package jour02.job04;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
