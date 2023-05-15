0e6a331975ea41e956fe920b9090556cc9de46f6
yoann-fermaud <yoann.fermaud@laplateforme.io>
2023-05-15 20:33:44 +0200

    [ADD] runtrack job01-day04

---
+++ src/jour04/job01/MyThread.java
@@ -0,0 +1,7 @@
+package jour04.job01;
+
+public class MyThread extends Thread{
+    public void run() {
+        System.out.println("My thread is in progress.");
+    }
+}
---
+++ src/jour04/job01/ThreadProject.java
@@ -0,0 +1,8 @@
+package jour04.job01;
+
+public class ThreadProject {
+    public static void main(String[] args) {
+        MyThread myThread = new MyThread();
+        myThread.run();
+    }
+}
